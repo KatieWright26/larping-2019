b3785a3c7ad4a208ce8f83ad2bf7e02c8098d870
Katie Wright <katie.wrght@gmail.com>
2019-09-13 13:00:39 +0200

    add links to edit larp and characters and add helper methods to render admin controls

--- app/models/larp.rb
+++ app/models/larp.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Larp < ApplicationRecord
-  has_one :address
+  has_one :address, dependent: :destroy
   belongs_to :user
   has_many :characters
 
   validates :title, :description, :start_date, presence: true
   accepts_nested_attributes_for :address
+
+  def belongs_to_current_user?(user)
+    user_id == user.id
+  end
 end
