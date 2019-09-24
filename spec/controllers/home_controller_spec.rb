8f6e50c813ba7e796d51f9bfb8875f5446f3dbb5
Katie Wright <katie.wrght@gmail.com>
2019-09-24 17:37:06 +0200

    add edit, update, destroy tests for larps and add handling for pundit policies

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,4 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe 'GET#index' do
+    let!(:larps) { FactoryBot.create_list(:larp, 10) }
+    it 'returns all LARPs' do
+      get :index
+
+      expect(response).to render_template(:index)
+
+      expect(assigns(:larps)).to eq(larps)
+    end
+  end
 end
